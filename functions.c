38c6c4afc5d09e351b178d4e831b02e633afc38f
Denise H. G <darcsis@gmail.com>
2010-11-17 22:05:05 +0800

    minor changes: modified copyright message.

--- functions.c
+++ functions.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2005-2007 Denise H. G. All rights reserved
+ * Copyright (c) 2005-2010 Denise H. G. All rights reserved
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
